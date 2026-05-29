416c4c62f8f11576528781c511108ae696363ed3
viesant <ricardo.viesant@gmail.com>
2026-05-29 20:39:50 +0200

    feature(backend): user mapper

---
+++ backend/src/main/java/com/travelplanner/backend/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package com.travelplanner.backend.mapper;
+
+import com.travelplanner.backend.dto.UserRequest;
+import com.travelplanner.backend.dto.UserResponse;
+import com.travelplanner.backend.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+  public User toEntity(UserRequest request) {
+    return new User(
+        request.name(),
+        request.email(),
+        request.password()
+    );
+  }
+
+  public UserResponse toResponseDto(User user) {
+    return new UserResponse(
+        user.getId(),
+        user.getName(),
+        user.getEmail()
+    );
+  }
+
+}
