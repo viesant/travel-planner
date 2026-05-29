1c4dabcaf2bcc2df66c287c119ecb48f0280a689
viesant <ricardo.viesant@gmail.com>
2026-05-29 20:16:32 +0200

    feature(backend): user entity

---
+++ backend/src/main/java/com/travelplanner/backend/entity/User.java
@@ -0,0 +1,35 @@
+package com.travelplanner.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password; //need criptography
+
+  public User(String name, String email, String password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+
+}
