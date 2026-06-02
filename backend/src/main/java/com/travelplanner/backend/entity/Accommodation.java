74a6f49c9abec6723d29bdb8f8b8197946300cb1
viesant <ricardo.viesant@gmail.com>
2026-06-03 00:08:46 +0200

    feat-backend(accommodation): entity implementation

---
+++ backend/src/main/java/com/travelplanner/backend/entity/Accommodation.java
@@ -0,0 +1,49 @@
+package com.travelplanner.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "accommodations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Accommodation {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column
+  private String location;
+
+  @Column(columnDefinition = "TEXT")
+  private String address;
+
+  @Column(name = "booking_number")
+  private String bookingNumber;
+
+  @Column(nullable = false, name = "check_in_date")
+  private LocalDate checkInDate;
+
+  @Column(nullable = false, name = "check_out_date")
+  private LocalDate checkOutDate;
+
+  @Column(precision = 10, scale = 2)
+  private BigDecimal price;
+
+  @Column(columnDefinition = "TEXT")
+  private String notes;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "trip_id", nullable = false)
+  private Trip trip;
+
+}
