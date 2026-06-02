417df109d68eba035cbeb314e21b9c721a1e929b
viesant <ricardo.viesant@gmail.com>
2026-06-03 00:50:39 +0200

    feat-backend(accommodation): basic repository implementation

--- backend/src/main/java/com/travelplanner/backend/repository/AccommodationRepository.java
+++ backend/src/main/java/com/travelplanner/backend/repository/AccommodationRepository.java
@@ -1,4 +1,13 @@
 package com.travelplanner.backend.repository;
 
-public interface AccommodationRepository {
+import com.travelplanner.backend.entity.Accommodation;
+import com.travelplanner.backend.entity.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AccommodationRepository extends JpaRepository<Accommodation,Long> {
+  List<Accommodation> findAllByTrip(Trip trip);
 }
