d2e2055a8c03d612f0b89730e6213d1b595516cd
viesant <ricardo.viesant@gmail.com>
2026-06-07 20:32:39 +0200

    chore-backend(doc): swagger setup implemented

--- backend/src/main/java/com/travelplanner/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/travelplanner/backend/config/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
                 auth
                     .requestMatchers(HttpMethod.POST, "/users").permitAll()
                     .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                    .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                     .anyRequest().authenticated()
         )
         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
