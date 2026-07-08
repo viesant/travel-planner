4b1b0825f9de60bf0f8266d0306a0787db7d24dd
viesant <ricardo.viesant@gmail.com>
2026-07-09 01:27:11 +0200

    feat-frontend(activities): interfaces implemented

---
+++ frontend/src/app/features/activities/models/activity-request.ts
@@ -0,0 +1,10 @@
+export interface ActivityRequest {
+  name: string;
+  location: string;
+  address: string;
+  startDateTime: string;
+  endDateTime: string;
+  bookingNumber: string;
+  price: number;
+  notes: string;
+}
